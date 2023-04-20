9fd76dfb47553743e8b695eb22dc54566c63c825
unknown <markyjeffersonsarmiento@gmail.com>
2023-04-20 08:48:25 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let age = parseInt(prompt("Please enter your age:"));
+
+if (age >= 25) {
+  alert("You are 25 years old and up.");
+} else {
+  alert("You are 24 years old and below");
+}
+
+let num = parseInt(prompt("Please enter a number:"));
+
+if (num < 100) {
+  alert("You entered a number less than 100.");
+} else {
+  alert("The number you entered is greater than or equal to 100.");
+}
